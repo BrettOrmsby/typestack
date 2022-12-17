b40c6701e64b05695bfa0ffab835a26adc467b49
BrettOrmsby <brettwormsby@outlook.com>
2022-12-17 10:57:55 -0500

    Feat: Attempt to make cli and npm package

---
+++ src/cli.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import typeStack from "./index.js";
+import {resolve} from "path"
+import fs from "fs"
+
+const [ , , filepath ] = process.argv;
+
+const absPath = resolve(process.cwd(), filepath);
+
+fs.readFile(absPath, 'utf8', function(err, data) {
+    if (err) throw err;
+    typeStack(data)
+  });
+  
