65a71976d57f7175e4b437f3aacc1ceacfaf5e0a
BrettOrmsby <brettwormsby@outlook.com>
2022-12-17 13:08:15 -0500

    Style: lint and format

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 
 import typeStack from "./index.js";
-import {resolve} from "path"
-import fs from "fs"
+import { resolve } from "path";
+import fs from "fs";
 
-const [ , , filepath ] = process.argv;
+const [, , filepath] = process.argv;
 
 const absPath = resolve(process.cwd(), filepath);
 
-fs.readFile(absPath, 'utf8', function(err, data) {
-    if (err) throw err;
-    typeStack(data)
-  });
-  
+fs.readFile(absPath, "utf8", function (err, data) {
+  if (err) throw err;
+  typeStack(data);
+});
