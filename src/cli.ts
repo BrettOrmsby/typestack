1868a6856e6db10008f070cc8d12226c6405218c
BrettOrmsby <brettwormsby@outlook.com>
2022-12-18 09:03:50 -0500

    Feat: Improve error for cli unreadable file

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import typeStack from "./index.js";
+import { consoleEffect, ConsoleEffects } from "./utils/consoleEffect.js";
 import { resolve } from "path";
 import fs from "fs";
 
@@ -9,6 +10,16 @@ const [, , filepath] = process.argv;
 const absPath = resolve(process.cwd(), filepath);
 
 fs.readFile(absPath, "utf8", function (err, data) {
-  if (err) throw err;
-  typeStack(data);
+  if (err) {
+    console.log(
+      consoleEffect("error", ConsoleEffects.FgRed, ConsoleEffects.Bright) +
+        ":" +
+        consoleEffect(
+          ` unable to read file at ${absPath}`,
+          ConsoleEffects.Bright
+        )
+    );
+  } else {
+    typeStack(data);
+  }
 });
