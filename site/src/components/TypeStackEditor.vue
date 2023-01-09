eedf575dbd9b69888feab7adb91d813490996374
BrettOrmsby <brettwormsby@outlook.com>
2023-01-09 17:05:33 -0500

    Feat: Can run TypeStack

--- site/src/components/TypeStackEditor.vue
+++ site/src/components/TypeStackEditor.vue
@@ -6,39 +6,41 @@ export default {
 
 <template>
   <PrismEditor class="my-editor" v-model="code" :highlight="highlight" />
+  <button @click="runCode(code)">Run</button>
 </template>
 
 <script lang="ts" setup>
 import { ref } from "vue";
 import { PrismEditor } from "vue-prism-editor";
 import Highlight from "../utils/highlight";
+import runCode from "../utils/runCode";
 import "vue-prism-editor/dist/prismeditor.min.css";
 
 const code = ref("55 33 -");
-const highlight = (input: string) => (new Highlight(input)).run();
+const highlight = (input: string) => new Highlight(input).run();
 </script>
 
 <style scoped>
 :deep() .token-number {
-    color: blue;
+  color: blue;
 }
 :deep() .token-str {
-    color: yellow;
+  color: yellow;
 }
 :deep() .token-bool {
-    color: purple;
+  color: purple;
 }
 :deep() .token-comment {
-    color: gray;
+  color: gray;
 }
 :deep() .token-keyword {
-    color: red;
+  color: red;
 }
 :deep() .token-identifier {
-    color: green;
+  color: green;
 }
 :deep() .token-punctuation {
-    color: white;
+  color: white;
 }
 /* required class */
 .my-editor {
---
+++ site/src/utils/runCode.ts
@@ -0,0 +1,31 @@
+import typestack from "typestack-lang/dist/index";
+import type { StackFunctions } from "typestack-lang/dist/functions";
+
+import math from "typestack-lang/dist/modules/Math";
+import date from "typestack-lang/dist/modules/Date";
+import str from "typestack-lang/dist/modules/Str";
+
+const functions: StackFunctions = {
+  int: {},
+  float: {},
+  str: {},
+  bool: {},
+  any: {},
+};
+addModule(math, "Math");
+addModule(date, "Date");
+addModule(str, "Str");
+
+export default async function run(code: string) {
+  typestack(code, functions);
+}
+
+function addModule(module: StackFunctions, name: string) {
+  Object.keys(module).forEach((type) =>
+    Object.keys(module[type as keyof StackFunctions]).forEach(
+      (func) =>
+        (functions[type as keyof StackFunctions][name + "." + func] =
+          module[type as keyof StackFunctions][func])
+    )
+  );
+}
